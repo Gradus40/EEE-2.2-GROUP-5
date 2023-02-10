0a31e315878b145c7ca28b5e14b08be9b646c0bc
Jeremycodes-web <123899729+Jeremycodes-web@users.noreply.github.com>
2023-02-10 15:42:50 +0300

    Add files via upload
    assignment question 2

--- main.c
+++ main.c
@@ -1,15 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-//A program that prints the alphabet in lowercase followed by a new line.(question 1)//
-
-int main(void)
-{
+//A program that prints the alphabet in lowercase, and then in uppercase,followed by a new line.(question 2)//
+int main(void) {
   char c;
+
   for (c = 'a'; c <= 'z'; c++) {
-    printf("%c", c);
+    putchar(c);
   }
-  printf("\n");
+
+  for (c = 'A'; c <= 'Z'; c++) {
+    putchar(c);
+  }
+
+  putchar('\n');
+
   return 0;
 }
+
+
+
 
