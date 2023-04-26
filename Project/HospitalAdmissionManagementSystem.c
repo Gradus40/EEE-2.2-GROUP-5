416a995640b820d7d16b295db66c5fd204dd833a
Peterngetich47 <123462285+Peterngetich47@users.noreply.github.com>
2023-04-26 10:38:38 +0300

    Update HospitalAdmissionManagementSystem.c

--- Project/HospitalAdmissionManagementSystem.c
+++ Project/HospitalAdmissionManagementSystem.c
@@ -182,3 +182,231 @@ void patientrecords() {
             // Wait for user input and go back to the main menu when a key is pressed
             printf("\n\t\t\tPress any key to go back to Main Menu.");
             getch();
+    }
+
+void searchpatient() {
+            clrscr();// Clear the screen and print the header for the patient records
+            printf("\t\t\t\t======Search Patient Record======\n\n\n");
+             // Declare a file pointer and a patient struct variable
+            FILE *fp;
+            struct patient info;
+            char name[50];
+            printf("\t\t\tEnter name to search: ");
+            scanf("%s", name);
+            int found = 0;
+
+             // Open the patient_info.txt file in read mode
+            fp = fopen("patient_info.txt", "r");
+
+            // Check if the file was successfully opened
+            if (fp == NULL) {
+            // If the file was not successfully opened, print an error message
+                printf("\t\t\tCan't Open File\n");
+            } else {
+                while (fscanf(fp, "%s %s %d %d %d %d %d %s %s %d %d %d %d %d\n", info.name, info.gender, &info.age, &info.height, &info.weight, &info.id, &info.bcn, info.hospital_admitted, info.wing_admitted, &info.doctor_fee, &info.lab_test_fee, &info.drug_fee, &info.arrival_date, &info.discharge_date) != EOF) {
+            if (strcmp(name, info.name) == 0) {
+                    printf("\n\t\t\t_______________________________________\n");
+                    printf("\t\t\tPatient Name : %s\n", info.name);
+                    printf("\t\t\tGender : %s\n", info.gender);
+                    printf("\t\t\tAge : %d\n", info.age);
+                    printf("\t\t\tHeight : %d cm\n", info.height);
+                    printf("\t\t\tWeight : %d kg\n", info.weight);
+                    if (info.id != 0) {
+                    printf("\t\t\tID : %d\n", info.id);
+                    } else {
+                    printf("\t\t\tBirth Certificate Number : %d\n", info.bcn);
+                    }
+                    printf("\t\t\tHospital Admitted : %s\n", info.hospital_admitted);
+                    printf("\t\t\tWing Admitted : %s\n", info.wing_admitted);
+                    printf("\t\t\tDoctor Fee : %d\n", info.doctor_fee);
+                    printf("\t\t\tLab Test Fee : %d\n", info.lab_test_fee);
+                    printf("\t\t\tDrug Fee : %d\n", info.drug_fee);
+                    printf("\t\t\tArrival Date : %d\n", info.arrival_date);
+                    printf("\t\t\tDischarge Date : %d\n", info.discharge_date);
+                    printf("\n\t\t\t_______________________________________\n");
+                    found = 1;
+                    break;
+                  }
+                }
+            if (!found) {
+                 printf("\n\t\t\tPatient not found.\n");
+                }
+            }
+                fclose(fp);
+          {
+            printf("\t\t\tPress any key to continue...\n");
+            getch();
+    }
+}
+void deletepatient() {
+        FILE *fp, *tempfp;
+        struct patient info;
+        char name[50];
+            // print header and prompt for patient name input
+        printf("\t\t\t\t======Delete Patient======\n\n\n");
+
+        printf("\t\t\tEnter the name of the patient: ");
+        scanf("%s", name);
+
+        fp = fopen("patient_info.txt", "r");
+        tempfp = fopen("temp.txt", "w");
+
+        if (fp == NULL) {
+            printf("\t\t\tCan't Open File\n");
+        } else {
+              // loop through each line of the file
+            while (fscanf(fp, "%s %s %d %d %d %d %d %s %s %d %d %d %d %d\n",info.name, info.gender, &info.age, &info.height, &info.weight, &info.id,&info.bcn, info.hospital_admitted, info.wing_admitted,&info.doctor_fee, &info.lab_test_fee, &info.drug_fee,&info.arrival_date, &info.discharge_date) != EOF) {
+               // if the patient name doesn't match the input, write the line to the temporary file
+                if (strcmp(info.name, name) != 0) {
+                    fprintf(tempfp, "%s %s %d %d %d %d %d %s %s %d %d %d %d %d\n",
+                            info.name, info.gender, info.age, info.height, info.weight, info.id,
+                            info.bcn, info.hospital_admitted, info.wing_admitted,
+                            info.doctor_fee, info.lab_test_fee, info.drug_fee,
+                            info.arrival_date, info.discharge_date);
+                }
+            }
+            fclose(fp);// close the original file
+            fclose(tempfp);// close the temporary file
+            remove("patient_info.txt");// delete the original file
+            rename("temp.txt", "patient_info.txt");// rename the temporary file to the original file name
+            printf("\t\t\tRecord Deleted\n");// print a message indicating successful deletion
+        }
+
+        printf("\t\t\tPress any key to continue...\n");
+        getch(); // wait for user input before continuing
+}
+
+void editrecord() {
+        FILE *fp, *tempfp; // declare two file pointers
+
+        struct patient info; // declare a struct for patient info
+
+        char name[50];// declare a char array for patient name input
+
+        int found = 0;
+
+        printf("\t\t\t\t======Edit Record======\n\n\n");
+        printf("\t\t\tEnter the name of the patient: ");
+        scanf("%s", name);
+         // open the file for reading
+        fp = fopen("patient_info.txt", "r");
+        // open a temporary file for writing
+
+        tempfp = fopen("temp.txt", "w");
+       // check if the file was successfully opened
+           if (fp == NULL) {
+            printf("\t\t\tCan't Open File\n");
+                } else {
+                  //reads the contents of the patient_info.txt file and assigns them to the struct patient object info
+                while (fscanf(fp, "%s %s %d %d %d %d %d %s %s %d %d %d %d %d\n",info.name, info.gender, &info.age, &info.height, &info.weight, &info.id,&info.bcn, info.hospital_admitted, info.wing_admitted,&info.doctor_fee, &info.lab_test_fee, &info.drug_fee,&info.arrival_date, &info.discharge_date) != EOF) {
+
+                if (strcmp(info.name, name) == 0) {
+                    found = 1;
+                    printf("\n\t\t\tWhat do you want to edit?\n");
+                    printf("\t\t\t1. Age\n");
+                    printf("\t\t\t2. Height\n");
+                    printf("\t\t\t3. Weight\n");
+                    printf("\t\t\t4. Hospital Admitted\n");
+                    printf("\t\t\t6. Wing Admitted\n");
+                    printf("\t\t\t7. Doctor Fee\n");
+                    printf("\t\t\t8. Lab Test Fee\n");
+                    printf("\t\t\t9. Drug Fee\n");
+                    printf("\t\t\t10. Arrival Date\n");
+                    printf("\t\t\t11. Discharge Date\n");
+                    printf("\t\t\t12. Go Back to Main Menu\n");
+                    printf("\n\t\t\tEnter your choice: ");
+                    int choice;
+                    scanf("%d", &choice);
+                   // allows the user to choose which field to edit.
+                    switch(choice) {
+                             // Update patient's age
+                          case 1:
+                                  printf("\t\t\tEnter new age: ");
+                                  scanf("%d", &info.age);
+                                  break;
+                                   // Update patient's height
+                          case 2:
+                                  printf("\t\t\tEnter new height: ");
+                                  scanf("%d", &info.height);
+                                  break;
+                                  // Update patient's weight
+                          case 3:
+                                  printf("\t\t\tEnter new weight: ");
+                                  scanf("%d", &info.weight);
+                                  break;
+                                  // Update patient's hospital admission information
+                          case 4:
+                                  printf("\t\t\tEnter new Hospital Admitted: ");
+                                  scanf("%s", info.hospital_admitted);
+                                  break;
+                                   // Update patient's wing admission information
+                          case 5:
+                                  printf("\t\t\tEnter new Wing Admitted: ");
+                                  scanf("%s", info.wing_admitted);
+                                  break;
+                                    //update patient's Doctor Fee
+                          case 6:
+                                  printf("\t\t\tEnter new Doctor Fee: ");
+                                  scanf("%d", &info.doctor_fee);
+                                  break;
+                                  // Update patient's Lab Test Fee
+                          case 7:
+                                  printf("\t\t\tEnter new Lab Test Fee: ");
+                                  scanf("%d", &info.lab_test_fee);
+                                  break;
+                                  // Update patient's Drug Fee
+                          case 8:
+                                  printf("\t\t\tEnter new Drug Fee: ");
+                                  scanf("%d", &info.drug_fee);
+                                  break;
+                          case 9:
+                                  printf("\t\t\tEnter new Arrival Date: ");
+                                  scanf("%d", &info.arrival_date);
+                                  break;
+                          case 10: // Update patient's Discharge Date
+                                  printf("\t\t\tEnter new Discharge Date: ");
+                                  scanf("%d", &info.discharge_date);
+                                  break;
+                          case 11:
+                                  printf("\t\t\tReturning to Main Menu\n");
+                                  break;
+                          default:
+                                printf("\t\t\tInvalid Choice!\n");
+                                break;
+                }
+                   fprintf(tempfp, "%s %s %d %d %d %d %d %s %s %d %d %d %d %d\n",info.name, info.gender, info.age, info.height, info.weight, info.id,info.bcn, info.hospital_admitted, info.wing_admitted, info.doctor_fee, info.lab_test_fee, info.drug_fee,info.arrival_date, info.discharge_date);
+
+                   } else {
+
+                   fprintf(tempfp, "%s %s %d %d %d %d %d %s %s %d %d %d %d %d\n",info.name, info.gender, info.age, info.height, info.weight, info.id,info.bcn, info.hospital_admitted, info.wing_admitted,info.doctor_fee, info.lab_test_fee, info.drug_fee,info.arrival_date, info.discharge_date);
+                   }
+           }
+            fclose(fp);
+            fclose(tempfp);
+
+            remove("patient_info.txt");
+            rename("temp.txt", "patient_info.txt");
+
+          if (found == 0) {
+            printf("\t\t\tRecord Not Found!\n");
+          } else {
+            printf("\t\t\tRecord Updated Successfully!\n");
+         }
+        fclose(fp);
+        fclose(tempfp);
+
+        remove("patient_info.txt");
+        rename("temp.dat", "patient_info.txt");
+
+        if (found == 0) {
+            printf("\t\t\tRecord Not Found!\n");
+        } else {
+            printf("\t\t\tRecord Updated Successfully!\n");
+          }
+    }
+
+        printf("\n\t\t\tPress any key to return to Main Menu");
+        getch(); // waits for user input
+}
+    
+             
